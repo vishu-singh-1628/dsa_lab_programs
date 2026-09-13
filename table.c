192467836d516fce4167e5ef2b003ffce025aac3
Vishu Kumar <vishukumar20071007@gmail.com>
2026-09-13 19:09:14 +0530

    Updated table  programs

--- table.c
+++ table.c
@@ -1,9 +1,15 @@
 # include<stdio.h>
 int main(){
     int n;
-    for (int  i = 0; i < count; i++)
-    {
-        /* code */
+    printf("Enter the number of rows: ");
+    scanf("%d",&n);
+    for (int  i = 0; i < n; i++)
+    { 
+        for (int j = 0; j < n; j++)
+        {
+            printf("%d ",(i+1)*(j+1));
+        }
+        printf("\n");
     }
     
 }
